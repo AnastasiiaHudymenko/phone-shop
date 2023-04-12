43dd5e5677ad3b2475997c12d3ed8d0ce29368af
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-04-12 12:17:00 +0300

    add styles to puchaseList

--- src/components/purchaseList/PurchaseList.jsx
+++ src/components/purchaseList/PurchaseList.jsx
@@ -1,3 +1,147 @@
+import { Map, Marker } from 'pigeon-maps';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchProductBasketDetalise } from 'redux/operations';
+import { clearBasketProduct } from 'redux/favoriteBasketSlice';
+import { getFavoriteBasket } from 'redux/selectors';
+import { Badge, Button } from 'react-bootstrap';
+import { IoPricetagsOutline } from 'react-icons/io5';
+import {
+  WrapMap,
+  InfoWindow,
+  PickupTitle,
+  ItemProduct,
+  TitleProduct,
+  PriceProduct,
+  ListProduct,
+  ThumbImg,
+  Container,
+  WrapContent,
+  WrapIcon,
+  TitleGoods,
+  MainContainer,
+  InputStyled,
+  SelectStyled,
+  FormStyled,
+} from './PurchaseList.styled';
+
 export const PurchaseList = () => {
-  return <div>PurchaseList</div>;
+  const [deliveryType, setDeliveryType] = useState('');
+
+  const isDisabled = deliveryType !== 'address';
+
+  const handleDeliveryChange = event => {
+    setDeliveryType(event.target.value);
+  };
+  const { idProductsBasket, basketProduct, isLoading } =
+    useSelector(getFavoriteBasket);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    idProductsBasket.map(id => dispatch(fetchProductBasketDetalise(id)));
+
+    return () => {
+      dispatch(clearBasketProduct());
+    };
+  }, [dispatch, idProductsBasket]);
+
+  return (
+    !isLoading && (
+      <MainContainer>
+        <Container>
+          <TitleGoods>
+            {' '}
+            <Badge bg="dark"> Your goods</Badge>
+          </TitleGoods>
+          <ListProduct>
+            {basketProduct.map(({ id, title, price, thumbnail }) => (
+              <ItemProduct key={id}>
+                <WrapContent>
+                  <ThumbImg>
+                    <img width={40} src={thumbnail} alt={title} />
+                  </ThumbImg>
+                  <TitleProduct>{title}</TitleProduct>
+                </WrapContent>
+                <WrapIcon>
+                  <IoPricetagsOutline color="#198754" size={20} />
+                  <PriceProduct>
+                    <Badge bg="dark"> {price}$</Badge>
+                  </PriceProduct>
+                </WrapIcon>
+              </ItemProduct>
+            ))}
+          </ListProduct>
+        </Container>
+        <div>
+          <Container>
+            <TitleGoods>
+              <Badge bg="dark">Form Checkout</Badge>
+            </TitleGoods>
+            <div>
+              <WrapMap>
+                <Map
+                  height={250}
+                  defaultCenter={[50.521327, 30.4462625]}
+                  defaultZoom={12}
+                >
+                  <Marker width={50} anchor={[50.521327, 30.4462625]} />
+                  <InfoWindow>
+                    <PickupTitle>Pickup from Kondratyuk 3</PickupTitle>
+                  </InfoWindow>
+                </Map>
+              </WrapMap>
+              <div>
+                <FormStyled>
+                  <TitleGoods>
+                    {' '}
+                    <Badge bg="dark">Choose a shipping method:</Badge>
+                  </TitleGoods>
+                  <div>
+                    <label>
+                      <SelectStyled
+                        value={deliveryType}
+                        onChange={handleDeliveryChange}
+                      >
+                        <option value="">--Choose method--</option>
+                        <option value="pickup">Pickup</option>
+                        <option value="address">Address</option>
+                      </SelectStyled>
+                    </label>
+                  </div>
+                  <InputStyled
+                    placeholder="Your name"
+                    type="text"
+                    name="name"
+                    disabled={isDisabled}
+                  />
+                  <InputStyled
+                    placeholder="Your last name"
+                    type="text"
+                    name="sureName"
+                    disabled={isDisabled}
+                  />
+                  <InputStyled
+                    placeholder="Your phone"
+                    type="tel"
+                    name="phone"
+                    disabled={isDisabled}
+                  />
+                  <InputStyled
+                    placeholder="Your addres"
+                    name="addresUser"
+                    disabled={isDisabled}
+                  />
+                  <TitleGoods>
+                    <Badge bg="success">To pay: 1000$</Badge>
+                  </TitleGoods>
+                  <Button type="submit" variant="outline-dark">
+                    Send
+                  </Button>
+                </FormStyled>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </MainContainer>
+    )
+  );
 };
---
+++ src/components/purchaseList/PurchaseList.styled.jsx
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const WrapMap = styled.div`
+  border-radius: 20px;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const InfoWindow = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 0 5px;
+  background-color: rgb(245 222 179 / 70%);
+  border-radius: 5px;
+  border-top-left-radius: 0;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+`;
+export const PickupTitle = styled.p`
+  margin: 0;
+  font-size: 12px;
+  font-style: italic;
+`;
+
+export const Container = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 26px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+`;
+export const ListProduct = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const WrapContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const WrapIcon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3px;
+`;
+
+export const ItemProduct = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ThumbImg = styled.div`
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+export const TitleProduct = styled.p`
+  margin: 0;
+`;
+
+export const PriceProduct = styled.p`
+  margin: 0;
+`;
+
+export const TitleGoods = styled.h3`
+  font-size: 18px;
+  font-style: italic;
+  margin-bottom: 20px;
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding: 30px 0;
+  @media screen and (min-width: 600px) {
+    width: 550px;
+  }
+`;
+
+export const InputStyled = styled.input`
+  height: 28px;
+  border-radius: 6px;
+  padding: 4 10px;
+  outline: none;
+  padding: 0 10px;
+  border: 1px solid grey;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+`;
+
+export const SelectStyled = styled.select`
+  height: 28px;
+  border-radius: 6px;
+  padding: 4 10px;
+  outline: none;
+  padding: 0 10px;
+  border: 1px solid grey;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+`;
+
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
